277aa04f21785f9f341ec3b30a8468a84cbaaa14
jared-joering <jjoering@gmail.com>
2025-10-11 08:50:12 -0400

    reworked entire blocks of code, studied, and then added error testing

--- api-script.py
+++ api-script.py
@@ -1,26 +1,35 @@
 import requests
+import json
 import psycopg2
+import os
+from datetime import date
+
 from config import api # calling the config file I created to hide my API
+
 from pokemontcgsdk import Card
-from pokemontcgsdk import Set
-from pokemontcgsdk import Type
-from pokemontcgsdk import Supertype
-from pokemontcgsdk import Subtype
-from pokemontcgsdk import Rarity    # pretty sure all of these will be used
 
 headers = {
     "X-API-Key": api
 }
 
-# fetch a single card by id (replace with any id you like)
-# card = Card.find("xy1-1")
+response = requests.get("https://api.pokemontcg.io/v2/cards", headers=headers)
+present = date.today().strftime("%Y-%m-%d")
+folder_path = "data/logs"
 
-print("card.id =", card.id)
-print("card.name =", card.name)
-print("type(card.subtypes) =", type(card.subtypes))
-print("card.subtypes =", card.subtypes)
-print(card.set.id)
-print(card.set.name)
-print(type(card.set.printedTotal))
-print(card.set.printedTotal)
-print(card.tcgplayer)
+# define the directory, the destination, and create if it's not there, otherwise: "Directory already present."
+# def mkdirectory():
+os.makedirs(folder_path, exist_ok=True)
+
+# def mkentry():
+log_path = os.path.join(folder_path, f"pmb_log_{present}.json")
+if response.status_code == 200: # only proceed if the HTTP request succeeds
+    try:
+        with open(log_path, "x") as f:
+            json.dump(response.json(), f, indent = 2)
+        print(f"Created log: {log_path}")
+    except FileExistsError:
+        print("Log already exists")
+    except ValueError:
+        print("Response did not contain valid JSON")
+    except Exception as error:
+        print("Unexpected error while writing log: ", error)
