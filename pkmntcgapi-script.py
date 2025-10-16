d72d55584b2acb1ea8a00533179ed43da9142b71
jared-joering <jjoering@gmail.com>
2025-10-16 14:13:02 -0400

    renamed the old 'api-script'; also completed both the retry block as well as modularize the code.  for all intents and purposes, this is done save for tweaks and improvements

---
+++ pkmntcgapi-script.py
@@ -0,0 +1,74 @@
+import requests
+import json
+import os
+import math
+import time
+from urllib3.util.retry import Retry
+from datetime import date
+
+from config import api # calling the config file I created to hide my API
+
+headers = { # setting the headers for the API request
+    "X-API-Key": api
+}
+
+present = date.today().strftime("%Y-%m-%d")
+folder_path = "data/logs"
+
+# define the directory, the destination, and create if it's not there, otherwise: "Directory already present."
+def mkdirectory():
+    os.makedirs(folder_path, exist_ok = True)
+
+def mkentries():
+    sesh = requests.Session() # creating a session to persist certain things across requests
+    retries = Retry(
+        total = 5,
+        backoff_factor = 1,
+        status_forcelist = [ 429, 500, 502, 503, 504 ],
+        allowed_methods = {"GET"},
+        respect_retry_after_header = True)
+    
+    sesh.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
+    sesh.mount('http://', requests.adapters.HTTPAdapter(max_retries=retries))   
+
+    response = sesh.get("https://api.pokemontcg.io/v2/cards", headers = headers)
+
+    starting_page = 38 # setting the page number
+
+    if response.status_code == 200: # only proceed if the HTTP request succeeds
+        data = response.json()
+        pageSize = data["pageSize"]
+        totalCount = data["totalCount"]
+
+        allPages = math.ceil(totalCount/pageSize)
+    
+        for pages in range(starting_page, allPages + 1):
+            step_response = sesh.get(f"https://api.pokemontcg.io/v2/cards?page={pages}&pageSize=250", headers = headers, timeout = (10.0, 90.0)) # a new response URL to include both page number and size
+            try:
+                log_path = os.path.join(folder_path, f"pmb_log_{present}-{pages}.json") # log file with pg number added
+                with open(log_path, "x") as f:
+                    json.dump(step_response.json(), f, indent = 2)
+                print(f"Created log: {log_path}")
+            
+                time.sleep(10.0)
+            
+            except FileExistsError:
+                print("Log already exists")
+            except ValueError:
+                print("Response did not contain valid JSON")
+            except Exception as error:
+                print("Unexpected error while writing log: ", error)
+
+if __name__ == "__main__":
+    mkdirectory()  # ensure the logs folder exists
+    mkentries()    # run the API scraping/logging
+
+'''# def JSON_merger(totalFiles, final_log):
+totalFiles = os.listdir(folder_path)
+merged_data = []
+for flog_path in totalFiles:
+    with open(flog_path, "r") as flog:
+        data = json.load(flog)
+        merged_data.append(data)
+    with open(f"final_log_{present}.json", "x") as final_log:
+        json.dump(merged_data, final_log)'''
