49b9d788f943211455209acffe7ca0caf4060551
jared-joering <jjoering@gmail.com>
2025-10-20 19:02:12 -0400

    added the db login for my postgresql that's hidden

--- database-upload.py
+++ database-upload.py
@@ -1,29 +1,41 @@
 import psycopg2
 import json
-import config
+from configparser import configparser
 
-conn = psycopg2.connect(
-    config
-)
+def config(filename="database.ini", section="postgresql"):
+    parser = configparser # creating parser
+    parser.read(filename) # reading the .ini file
 
-user_file=input("Please choose the file you wish to enter into the database: ")
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+        
+    else:
+        raise Exception("Section {0} not found in the {1} file".format(section, filename))
+    
+    return db
 
-with open(user_file, "r", encoding="utf-8") as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        row = json.loads(line)
+def psyco_path()
+    user_file=input("Please choose the file you wish to enter into the database: ")
 
-        for card in row.get("data", []):
-            card_id = card["id"]
-            name = card["name"]
-            supertype = card["supertype"]
-            subtypes = card.get("subtypes", None)
-            set_object = card.get("set", {})
-            set_series = set_object.get("series", None)
-            card_number = card.get("number", None)
-            set_pt = set_object.get("printedTotal", None)
-            artist = card.get("artist", None)
-            rarity = card.get("rarity", None)
+    with open(user_file, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            row = json.loads(line)
+
+            for card in row.get("data", []):
+                card_id = card["id"]
+                name = card["name"]
+                supertype = card["supertype"]
+                subtypes = card.get("subtypes", None)
+                set_object = card.get("set", {})
+                set_series = set_object.get("series", None)
+                card_number = card.get("number", None)
+                set_pt = set_object.get("printedTotal", None)
+                artist = card.get("artist", None)
+                rarity = card.get("rarity", None)
 
