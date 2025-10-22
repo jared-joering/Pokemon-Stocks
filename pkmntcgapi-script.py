94a5340349aac5abfe2843a670f3f68b5c7884a3
jared-joering <jjoering@gmail.com>
2025-10-21 20:46:55 -0400

    added comments, elaborated on some of the more complicated processes

--- pkmntcgapi-script.py
+++ pkmntcgapi-script.py
@@ -18,16 +18,30 @@ headers = { # setting the headers for the API request
 present = date.today().strftime("%Y-%m-%d")
 folder_path = f"data/logs/{present} log"
 
-# define the directory, the destination, and create if it's not there, otherwise: "Directory already present."
+'''
+Define the directory, the destination, and create if it's not there,
+otherwise: "Directory already present."
+'''
+
 def mkdirectory():
     os.makedirs(folder_path, exist_ok = True)
+
+'''
+We're pinging the API after opening a 'session' which allows us to 
+maintain a state of contact with the API.
+
+From there, we draw on each page of 250 up to a total of 79 -
+allPages - [for the moment (10/21)] and create logs of each of
+them.  After that, we move on to the next until the end.  Admidst
+all of that, we're checking for errors.
+'''
 
 def mkentries():
     sesh = requests.Session() # creating a session to persist certain things across requests
     retries = Retry( # creating the retry vehicle
-        total = 5,
-        backoff_factor = 1,
-        status_forcelist = [ 429, 500, 502, 503, 504 ],
+        total = 5, # Total retries 
+        backoff_factor = 1, # We back off exponentially, five times (1, 2, 4, 8, 16 seconds)
+        status_forcelist = [ 429, 500, 502, 503, 504 ], 
         allowed_methods = {"GET"},
         respect_retry_after_header = True)
     
@@ -36,24 +50,24 @@ def mkentries():
 
     response = sesh.get("https://api.pokemontcg.io/v2/cards", headers = headers)
 
-    starting_page = 1 # setting the page number
+    starting_page = 1 # setting the starting page number
 
     if response.status_code == 200: # only proceed if the HTTP request succeeds
         data = response.json()
-        pageSize = data["pageSize"]
-        totalCount = data["totalCount"]
+        pageSize = data["pageSize"] # taking the page size...
+        totalCount = data["totalCount"] # ... and the total amount of 'cards'...
 
-        allPages = math.ceil(totalCount/pageSize) # calculating total pages for the loop
+        allPages = math.ceil(totalCount/pageSize) # ... and then calculating total pages for the loop
     
         for pages in range(starting_page, allPages + 1):
             step_response = sesh.get(f"https://api.pokemontcg.io/v2/cards?page={pages}&pageSize=250", headers = headers, timeout = (10.0, 90.0)) # a new response URL to include both page number and size
             try:
                 log_path = os.path.join(folder_path, f"pmb_log_{present}-{pages}.json") # log file with pg number added
                 with open(log_path, "x") as f:
-                    json.dump(step_response.json(), f, indent = 2)
+                    json.dump(step_response.json(), f, indent = 2) # creating the log
                 print(f"Created log: {log_path}")
             
-                time.sleep(10.0)
+                time.sleep(10.0) # giving the grace of a 10 second wait so the server doesn't bog down on our accord
             
             except FileExistsError: # running a few important error checkers and a general error
                 print("Log already exists")
