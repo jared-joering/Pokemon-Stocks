10ed4f452566c8a0a61f43589953bc4c3440d8ef
jared-joering <jjoering@gmail.com>
2025-10-24 18:21:35 -0400

    created from a hard copy of database-upload.py as their aims are the same just with different tables

---
+++ prices-upload.py
@@ -0,0 +1,91 @@
+import os
+import json
+import datetime as dt
+import configparser as ConfigParser
+import psycopg2
+
+'''
+Do a similar deal as we did with the database-upload
+'''
+
+def config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser() # creating parser
+    parser.read(filename) # reading the .ini file
+    db = {} # empty dictionary for database
+
+    if parser.has_section(section): # checking if a config section exists
+        params = parser.items(section) # "
+        for param in params: # reading every setting
+            db[param[0]] = param[1] # applying these for later use
+        
+    else:
+        raise Exception("Section {0} not found in the {1} file".format(section, filename))
+    
+    try:
+        conn = psycopg2.connect(**db) # connecting to the db by bypassing the dictionary
+        print("Database connected successfully.")
+    except:
+        print("Database not connected successfully.")
+        raise
+
+    return conn
+
+'''
+Again, much like the database-upload.py, we will be prompting the 
+user for a file to insert into the database.  If they don't select
+it, we kill the operation.  From there, we utilize the conn(ect) we
+created earlier and create a cursor that allows us to insert all of
+the files that we literate through before cutting and closing down.
+'''
+
+def psyco_price_path():
+    user_file = input("Enter the full (or relative) path to the NDJSON file to merge: ")
+
+    if not user_file:
+        print("No file selected, exiting.")
+        return
+
+    conn = config() # connecting to db
+    cur = conn.cursor() # creating the cursor
+
+    insert_query = """INSERT INTO price (id, source, pricing_date, variant, price, source_url, raw, created on)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+
+    with open (user_file, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            row = json.loads(line)
+
+            for card in row.get("data", []): # literating through all the 'card' objects
+                card_id = card["id"]
+                subtypes = card.get("subtypes") or [] # subtypes can be multiple or there can be none -- this also allows for us to more easily search with SQL
+                set_object = card.get("set", {})
+                set_series = set_object.get("series", None) # as with subtypes and the others below - just in case - there could be empty results from this, so we account for it
+                card_number = card.get("number", None)
+                set_pt = set_object.get("printedTotal", None)
+                artist = card.get("artist", None)
+                rarity = card.get("rarity", None)
+
+                cur.execute(insert_query, ( # writing said 'card' objects
+                    card_id,
+                    name,
+                    supertype,
+                    subtypes,
+                    set_object.get("name", None),
+                    set_series,
+                    card_number,
+                    set_pt,
+                    artist,
+                    rarity
+                ))
+    
+    conn.commit() # wrapping up
+    cur.close()
+    conn.close()
+    print("Finished inserting records into database, closing connection.")
+
+if __name__ == "__main__": # calling
+    psyco_path()
